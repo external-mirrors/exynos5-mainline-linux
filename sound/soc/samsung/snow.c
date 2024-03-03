d66200c5c8b34c00363905cca3351beb046ec44d
Anton Bambura <jenneron@postmarketos.org>
2024-03-03 23:10:13 +0200

    ASoC: samsung: snow: add support for max98089
    Signed-off-by: Anton Bambura <jenneron@protonmail.com>

--- sound/soc/samsung/snow.c
+++ sound/soc/samsung/snow.c
@@ -326,6 +326,7 @@ static void snow_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id snow_of_match[] = {
+	{ .compatible = "google,snow-audio-max98089", },
 	{ .compatible = "google,snow-audio-max98090", },
 	{ .compatible = "google,snow-audio-max98091", },
 	{ .compatible = "google,snow-audio-max98095", },
