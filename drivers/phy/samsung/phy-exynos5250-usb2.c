2601ef8a0a44946015a863ca0d36ece6165d2e81
Alexandre Marquet <tb@a-marquet.fr>
2025-02-25 00:21:15 +0100

    phy: samsung: exynos5250-usb2: Fix USB2 OTG mode (v2)
    The following changes are needed for USB2 of Exynos 5250 devices to work
    in OTG mode.
    
    In exynos5250_power_on(), an additional reset of host ctrl0 register is
    necessary.
    
    --
    
    Changes in v2:
    
    In exynos5250_isol(), the PMU offset must always be
    EXYNOS5250_USB_ISOL_HOST_OFFSET.
    
    Signed-off-by: Alexandre Marquet <tb@a-marquet.fr>

--- drivers/phy/samsung/phy-exynos5250-usb2.c
+++ drivers/phy/samsung/phy-exynos5250-usb2.c
@@ -178,10 +178,8 @@ static void exynos5250_isol(struct samsung_usb2_phy_instance *inst, bool on)
 	u32 mask = EXYNOS_5250_USB_ISOL_ENABLE;
 
 	if (drv->cfg == &exynos5250_usb2_phy_config &&
-	    inst->cfg->id == EXYNOS5250_DEVICE)
-		offset = EXYNOS_5250_USB_ISOL_OTG_OFFSET;
-	else if (drv->cfg == &exynos5250_usb2_phy_config &&
-		 inst->cfg->id == EXYNOS5250_HOST)
+		 (inst->cfg->id == EXYNOS5250_HOST ||
+		  inst->cfg->id == EXYNOS5250_DEVICE))
 		offset = EXYNOS_5250_USB_ISOL_HOST_OFFSET;
 	else if (drv->cfg == &exynos5420_usb2_phy_config &&
 		 inst->cfg->id == EXYNOS5250_HOST)
@@ -233,6 +231,24 @@ static int exynos5250_power_on(struct samsung_usb2_phy_instance *inst)
 			EXYNOS_5250_USBOTGSYS_OTGDISABLE);
 		writel(otg, drv->reg_phy + EXYNOS_5250_USBOTGSYS);
 
+		/* Reset */
+		ctrl0 &=	~(EXYNOS_5250_HOSTPHYCTRL0_PHYSWRST |
+				EXYNOS_5250_HOSTPHYCTRL0_PHYSWRSTALL |
+				EXYNOS_5250_HOSTPHYCTRL0_SIDDQ |
+				EXYNOS_5250_HOSTPHYCTRL0_FORCESUSPEND |
+				EXYNOS_5250_HOSTPHYCTRL0_FORCESLEEP);
+		ctrl0 |=	EXYNOS_5250_HOSTPHYCTRL0_LINKSWRST |
+				EXYNOS_5250_HOSTPHYCTRL0_UTMISWRST |
+				EXYNOS_5250_HOSTPHYCTRL0_COMMON_ON_N;
+		writel(ctrl0, drv->reg_phy + EXYNOS_5250_HOSTPHYCTRL0);
+		udelay(10);
+		ctrl0 &=	~(EXYNOS_5250_HOSTPHYCTRL0_LINKSWRST |
+				EXYNOS_5250_HOSTPHYCTRL0_UTMISWRST);
+		writel(ctrl0, drv->reg_phy + EXYNOS_5250_HOSTPHYCTRL0);
+
+		/* The following delay is necessary for the reset sequence to be
+		* completed */
+		udelay(80);
 
 		break;
 	case EXYNOS5250_HOST:
